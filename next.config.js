c08bc03f01c04839d23680f8f34a20e0aae140d7
mrohitsingh <45506782+mrohitsingh@users.noreply.github.com>
2024-10-10 19:26:05 +0000

    custom path format

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@ const nextConfig = {
   },
   assetPrefix: 'https://mrohitsingh.in/',
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/css/:slug*', // Your custom path format
+        destination: '/_next/static/css/:slug*', // The original path where the file is actually located
+      },
+      {
+        source: '/js/:slug*', // Your custom path format
+        destination: '/_next/static/js/:slug*', // The original path where the file is actually located
+      },
+      {
+        source: '/js/:slug*', // Your custom path format
+        destination: '/_next/static/chunks/:slug*', // The original path where the file is actually located
+      },
+      {
+        source: '/pages/:slug*', // Your custom path format
+        destination: '_next/static/chunks/pages/:slug*', // The original path where the file is actually located
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
